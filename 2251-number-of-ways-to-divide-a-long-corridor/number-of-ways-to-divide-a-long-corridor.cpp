e4bd3d5bb348b64b4c91835ab3dac3e33aeae716
Parmesh Girdonia <127977133+parmesh-04@users.noreply.github.com>
2026-03-20 12:51:50 +0530

    Time: 16 ms (83.70%) | Memory: 58.8 MB (24.09%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     int mod = 1e9+7;
+    int numberOfWays(string corridor) {
+        vector<int> pos;
+        for (int i=0; i<corridor.size();i++) {
+            if (corridor[i]=='S') {
+                pos.push_back(i);
+            }
+        }
+        
+        if (pos.size()%2!=0 || pos.size()==0) {
+            return 0;
+        }
+        
+        long long ans=1;
+        for (int i=2;i<pos.size();i+=2) {
+            int gap=pos[i]-pos[i-1];
+            ans=(ans*gap)%mod;
+        }
+
+        return ans;
+    }
+};
